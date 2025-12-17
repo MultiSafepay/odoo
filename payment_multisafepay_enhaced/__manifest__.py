dcbb5997c70faa953c2ed3a6704ff19967b718e4
Marco Antonio Gil <marco.gil@multisafepay.com>
2025-12-17 15:43:15 +0100

    Update payment_multisafepay_enhaced/__manifest__.py

--- payment_multisafepay_enhaced/__manifest__.py
+++ payment_multisafepay_enhaced/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://github.com//multisafepay",
     "license": "AGPL-3",
     "category": "Accounting/Payment Providers",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.0.0",
     "application": True,
     "depends": [
         "payment_multisafepay",
