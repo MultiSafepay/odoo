b7c2ab5f2233bdde5fe6d3cced27ac60ad20420d
Marco Antonio Gil <marco.gil@multisafepay.com>
2025-12-17 14:00:57 +0100

    Update payment_multisafepay/models/payment_method.py

--- payment_multisafepay/models/payment_method.py
+++ payment_multisafepay/models/payment_method.py
@@ -232,7 +232,6 @@ class PaymentMethod(models.Model):
         # Return all non-MultiSafepay methods plus filtered MultiSafepay methods
         return other_methods + allowed_multisafepay_methods
 
-    # Note: _filter_by_pricelist() method moved to payment_multisafepay_enhaced module (custom feature)
 
     # ===================================
     # MULTISAFEPAY - Availability Report
