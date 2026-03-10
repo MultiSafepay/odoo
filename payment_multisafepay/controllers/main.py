5b1545e619efeaf77c78b19f0141e130f98543f3
Marco Antonio Gil <marco.gil@multisafepay.com>
2026-03-10 12:02:59 +0100

    Release of 18.0.2.1.0

--- payment_multisafepay/controllers/main.py
+++ payment_multisafepay/controllers/main.py
@@ -875,7 +875,7 @@ class MultiSafepayController(http.Controller):
         # Create a Plugin object with the necessary details
         plugin = (
             Plugin(**{})
-            .add_plugin_version("2.0.0")
+            .add_plugin_version("2.1.0")
             .add_shop("Odoo")
             .add_shop_version("19.0")
             .add_shop_root_url(url)
