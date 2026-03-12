8c89ff79a9a55beb4f776b9566ddd8948933e092
Daniel Civit <daniel.civit@multisafepay.com>
2026-03-12 15:51:15 +0100

    Release of 19.0.2.1.1

--- payment_multisafepay_enhanced/__manifest__.py
+++ payment_multisafepay_enhanced/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://github.com//multisafepay",
     "license": "AGPL-3",
     "category": "Sales",
-    "version": "19.0.2.1.0",
+    "version": "19.0.2.1.1",
     "application": True,
     "depends": [
         "payment_multisafepay",
