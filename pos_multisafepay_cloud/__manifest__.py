50627305e68ad2e2777e28ce92b0cb6e1e4f7f91
Marco Antonio Gil <marco.gil@multisafepay.com>
2026-06-22 11:07:37 +0200

    ODOO-177: Update author field in manifest to remove OCA reference

--- pos_multisafepay_cloud/__manifest__.py
+++ pos_multisafepay_cloud/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "PoS MultiSafepay Cloud",
     "summary": "PoC for MultiSafepay Cloud POS payments in Odoo POS",
-    "author": "MultiSafepay, Odoo Community Association (OCA)",
+    "author": "MultiSafepay",
     "website": "https://github.com//multisafepay",
     "license": "AGPL-3",
     "category": "Point of Sale",
