5c38a2a6a9132a26da7522c4996bd7fda9c56a36
Daniel Civit <daniel.civit@multisafepay.com>
2026-07-03 16:56:26 +0200

    Update pos_multisafepay_cloud/tests/__init__.py
    Co-authored-by: Marco Antonio Gil <marco.gil@multisafepay.com>

--- pos_multisafepay_cloud/tests/__init__.py
+++ pos_multisafepay_cloud/tests/__init__.py
@@ -1,1 +1,6 @@
+# Copyright (c) MultiSafepay, Inc. All rights reserved.
+# This file is licensed under the GNU Affero General Public License (AGPL) version 3.0.
+# See the LICENSE.md file for more information.
+# See the DISCLAIMER.md file for disclaimer details
+
 from . import test_main_controller
