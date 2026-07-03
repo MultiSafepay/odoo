d7c5f9d4a690c2229f94d941b986cdc6a1b20348
Daniel Civit <daniel.civit@multisafepay.com>
2026-07-03 16:56:18 +0200

    Update pos_multisafepay_cloud/tests/test_main_controller.py
    Co-authored-by: Marco Antonio Gil <marco.gil@multisafepay.com>

--- pos_multisafepay_cloud/tests/test_main_controller.py
+++ pos_multisafepay_cloud/tests/test_main_controller.py
@@ -1,3 +1,8 @@
+# Copyright (c) MultiSafepay, Inc. All rights reserved.
+# This file is licensed under the GNU Affero General Public License (AGPL) version 3.0.
+# See the LICENSE.md file for more information.
+# See the DISCLAIMER.md file for disclaimer details
+
 from unittest.mock import MagicMock, patch
 
 from odoo.tests.common import HttpCase, TransactionCase
