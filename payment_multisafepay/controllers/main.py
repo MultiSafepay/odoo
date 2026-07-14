6e72e655be7550253af37fd83f8b7a8f44dfb623
Daniel Civit <daniel.civit@multisafepay.com>
2026-07-14 14:22:13 +0200

    Release of 19.0.2.3.0

--- payment_multisafepay/controllers/main.py
+++ payment_multisafepay/controllers/main.py
@@ -885,7 +885,7 @@ class MultiSafepayController(http.Controller):
         # Create a Plugin object with the necessary details
         plugin = (
             Plugin(**{})
-            .add_plugin_version("2.2.3")
+            .add_plugin_version("2.3.0")
             .add_shop("Odoo")
             .add_shop_version("19.0")
             .add_shop_root_url(url)
--- payment_multisafepay_enhanced/__manifest__.py
+++ payment_multisafepay_enhanced/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://github.com//multisafepay",
     "license": "AGPL-3",
     "category": "Sales",
-    "version": "19.0.2.2.3",
+    "version": "19.0.2.3.0",
     "application": True,
     "depends": [
         "payment_multisafepay",
